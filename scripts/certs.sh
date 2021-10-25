516fdedd6b537c303dc698080c22a07c759af7c5
Joseph <joseph@titaniumcoast.com>
2021-10-25 23:45:44 +0000

    Path Fixes?

--- scripts/certs.sh
+++ scripts/certs.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-cd ../testing_nginx/certs
+openssl req -x509 -new -newkey rsa:4096 -nodes -keyout ../testing_nginx/certs/myCA.key -sha256 -days 1825 -out ../testing_nginx/certs/myCA.pem -subj "/CN=Automation!"
 
-openssl req -x509 -new -newkey rsa:4096 -nodes -keyout myCA.key -sha256 -days 1825 -out myCA.pem -subj "/CN=Automation!"
+openssl req -new -newkey rsa:4096 -nodes -keyout ../testing_nginx/certs/fnfcentral.com.key -out ../testing_nginx/certs/fnfcentral.com.csr -subj "/CN=FNFCentral FAKE"
 
-openssl req -new -newkey rsa:4096 -nodes -keyout fnfcentral.com.key -out fnfcentral.com.csr -subj "/CN=FNFCentral FAKE"
-
-openssl x509 -req -in fnfcentral.com.csr -CA myCA.pem -CAkey myCA.key -CAcreateserial -out fnfcentral.com.crt -days 825 -sha256 -extfile ../fnfcentral.com.ext
+openssl x509 -req -in ../testing_nginx/certs/fnfcentral.com.csr -CA ../testing_nginx/certs/myCA.pem -CAkey ../testing_nginx/certs/myCA.key -CAcreateserial -out ../testing_nginx/certs/fnfcentral.com.crt -days 825 -sha256 -extfile ../testing_nginx/certs/fnfcentral.com.ext
