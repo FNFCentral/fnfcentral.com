dcf9941d20afa0a7e74a5565d9915e8a16630d10
JRed <joseph@titaniumcoast.com>
2021-12-19 02:29:11 -0500

    Added A Warning For Local and Canary

---
+++ website/src/lib/Warning.svelte
@@ -0,0 +1,43 @@
+<script>
+    import Fa from "svelte-fa/src/fa.svelte";
+    import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
+
+    import { local, canary } from "$lib/modeData";
+</script>
+
+<div id="warning" class="warningBox">
+    <div id="top">
+        <Fa icon={faExclamationTriangle} style="display: inline" />
+        <b>Warning!</b>
+    </div>
+    <div id="text">
+        {#if local}
+            <p>
+                This site is a local site, run by an individual. Many features
+                may not work or not be implemented. DO NOT ENTER ANY REAL
+                CREDENTIALS!
+            </p>
+        {:else if canary}
+            <p>
+                This site is a canary site, run as a test. Many features may not
+                work or not be implemented. Expect consistant deletion of data.
+                Your real credentials will not work here. Out of an abundence of
+                caution, please use a password you do not use anywhere else (you
+                should already do that)!
+            </p>
+        {/if}
+    </div>
+</div>
+
+<style>
+    .warningBox {
+        background-color: #ffcc00;
+        color: #000000;
+        font-family: domine;
+        font-size: 16px;
+    }
+
+    .warningBox b {
+        font-size: 20px;
+    }
+</style>
--- website/src/routes/__layout.svelte
+++ website/src/routes/__layout.svelte
@@ -1,6 +1,9 @@
 <script>
     import Header from "$lib/Header.svelte";
     import Footer from "$lib/Footer.svelte";
+    import Warning from "$lib/Warning.svelte";
+
+    import { local, canary } from "$lib/modeData";
 
     import "../app.css";
     import "../sweetAlert.scss";
@@ -8,6 +11,10 @@
 
 <div class="flex flex-col hScreenPlus">
     <div class="justify-self-start"><Header /></div>
+
+    {#if local || canary}
+        <div class="justify-self-start"><Warning /></div>
+    {/if}
 
     <div class="flex-grow justify-self-start"><slot /></div>
 
